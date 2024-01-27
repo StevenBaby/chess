dc58b3401ff60f564f4e06460124acb62272c2bb
Steven Kang <kangweibaby@163.com>
2024-01-28 02:34:04 +0800

    add version

---
+++ src/version.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+VERSION = '3.0.0'
+__VERSION__ = VERSION
+
+
+def increase():
+    import os
+    import re
+    filename = os.path.abspath(__file__)
+    with open(filename, encoding='utf8') as file:
+        content = file.read()
+
+    match = re.search(r"VERSION = '(\d+).(\d+).(\d+)'", content)
+    old = f'{match.group(1)}.{match.group(2)}.{match.group(3)}'
+    new = f'{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}'
+
+    content = content.replace(old, new)
+
+    with open(filename, 'w', encoding='utf8') as file:
+        file.write(content)
